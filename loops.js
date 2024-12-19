56bfb1fabda82afe725259263c04b2f9cc0433a6
MatthieuMosch <mmosch@gmail.com>
2024-12-19 16:49:48 +0100

    loops.js complete

--- loops.js
+++ loops.js
@@ -10,10 +10,10 @@
 // Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen...
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
-
-
-
-
+console.log("Opdracht 1");
+for (let i = 1; i < 6; i++) { //loop from 1 bcause firts line already has 1 *
+    console.log("*".repeat(i)); //another option would have been to loop from 0 and use .repeat(i+1) on this line
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de laatste (vijfde) loop het woord "klaar!"
@@ -24,10 +24,22 @@
 // loop...
 // klaar!
 // ==========================================
+console.log("Opdracht 2");
+/*
+for (let i = 0; i < 5; i++) { //this is a version with if..else which i think is being asked
+    if (i<4) {
+        console.log("loop...");
+    } else{
+        console.log("klaar!");
+    }
+}
+*/
 
-
-
-
+//the code in comment above is what is being asked, but this is a better version with the same result
+for (let i = 0; i < 4; i++) {
+    console.log("loop...");
+}
+console.log("klaar!");
 
 // ==========================================
 // Opdracht 3: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -44,10 +56,10 @@
 // >> 8
 // >> 9
 // ==========================================
-
-
-
-
+console.log("Opdracht 3");
+for (let i = 0; i < 9; i++) {
+    console.log(">".repeat(i / 3) + i);
+}
 
 // ==========================================
 // Opdracht 4 (BONUS): schrijf een for-loop die van 0 tot 100 loopt en de getallen print.
@@ -86,6 +98,16 @@
 // FizzBuzz
 // etc.
 // ==========================================
-
+console.log("Opdracht 4");
+for (let i = 0; i < 100; i++) { //0 tot 100 is including 0 and without 100, the example output is from 1 to 100
+    let outputString = "";
+    if (i%3===0) outputString = "Fizz";
+    if (i%5===0) outputString += "Buzz";
+    if (outputString==="") {
+        console.log(i);
+    } else{
+        console.log(outputString);
+    }
+}
 
 
